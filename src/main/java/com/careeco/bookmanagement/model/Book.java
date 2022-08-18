6a9f1b651274489ddb7662406310f3fa44a2614f
Satti babu <sattibabuchittala@gmail.com>
2022-08-18 03:15:49 -0400

    IVM-12 | created Book Model

---
+++ src/main/java/com/careeco/bookmanagement/model/Book.java
@@ -0,0 +1,42 @@
+package com.careeco.bookmanagement.model;
+
+public class Book {
+
+
+    private String bookId;
+    private String bookName;
+    private double bookPrice;
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public double getBookPrice() {
+        return bookPrice;
+    }
+
+    public void setBookPrice(double bookPrice) {
+        this.bookPrice = bookPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookId='" + bookId + '\'' +
+                ", bookName='" + bookName + '\'' +
+                ", bookPrice=" + bookPrice +
+                '}';
+    }
+}
